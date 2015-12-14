c0ad477ec4df977bb32504173668ce36bd7b6754
LIU Wei <liuwei@nbu.edu.cn>
2015-12-14 23:32:33 +0800

    para text builder is OK

--- BCPrice_Catcher/Trader/HuobiTrader.cs
+++ BCPrice_Catcher/Trader/HuobiTrader.cs
@@ -17,8 +17,9 @@ namespace BCPrice_Catcher.Trader
         private string postUrl = "https://api.huobi.com/apiv3";
         private const string AccessKey = "48a8ded8-963359c3-1aaa1237-51909";
         private const string SecretKey = "57c362c4-2462acb0-23369fb2-469b8";
-        private WebClient _client = new WebClient();
+        private readonly WebClient _client = new WebClient();
         private const string Market = "cny";
+
 
         public HuobiTrader()
         {
@@ -28,14 +29,55 @@ namespace BCPrice_Catcher.Trader
 
         public override string GetAccountInfo()
         {
-            DateTime dateTime = DateTime.Now;
-            string plainText = $"access_key={AccessKey}&created={Convertor.ConvertDateTimeToJsonTimeStamp(dateTime)}&method=get_account_info&secret_key={SecretKey}";
-            string sign = Convertor.ConvertPlainTextToMd5Value(plainText);
-            string paraString =
-                $"access_key={AccessKey}&created={Convertor.ConvertDateTimeToJsonTimeStamp(dateTime)}&market={Market}&method=get_account_info&sign={sign}";
-            //return sign;
-            return Encoding.UTF8.GetString(_client.UploadData(postUrl, Encoding.UTF8.GetBytes(paraString)));
+            var builder = new HuobiParasTextBuilder();
+
+
+            builder.Parameters.Add("method", "get_account_info");
+            builder.Parameters.Add("market", Market);
+
+            string parasText = builder.GetPostDataString(new[] {"created", "secret_key", "method", "access_key"});
+            return Encoding.UTF8.GetString(_client.UploadData(postUrl, Encoding.UTF8.GetBytes(parasText)));
+        }
+
+        public class HuobiParasTextBuilder
+        {
+            public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+
+            public HuobiParasTextBuilder()
+            {
+                Parameters.Add("access_key", AccessKey);
+                Parameters.Add("secret_key", SecretKey);
+                Parameters.Add("created", Convertor.ConvertDateTimeToJsonTimeStamp(DateTime.Now).ToString());
+            }
+
+            public string GetPostDataString(string[] paraNamesToEncrypt)
+            {
+                var parasForSign = (from p in Parameters
+                    where paraNamesToEncrypt.Contains(p.Key)
+                    orderby p.Key
+                    select p).ToList();
+
+                StringBuilder plainTextBuilder = new StringBuilder();
+                foreach (var v in parasForSign)
+                {
+                    plainTextBuilder.Append(v.Key).Append("=").Append(v.Value).Append("&");
+                }
+                //Remove the "&" at the end
+                string signText =
+                    Convertor.ConvertPlainTextToMd5Value(
+                        plainTextBuilder.Remove(plainTextBuilder.Length - 1, 1).ToString());
+
+                //build the final paras string for POST
+                StringBuilder parasTextBuilder = new StringBuilder();
+                foreach (var v in Parameters)
+                {
+                    if (v.Key != "secret_key")
+                    {
+                        parasTextBuilder.Append(v.Key).Append("=").Append(v.Value).Append("&");
+                    }
+                }
+                return parasTextBuilder.Append("sign=").Append(signText).ToString();
+            }
         }
     }
 }
-
--- BCPrice_Catcher/Util/Convertor.cs
+++ BCPrice_Catcher/Util/Convertor.cs
@@ -13,12 +13,15 @@ namespace BCPrice_Catcher.Util
     {
         public static DateTime ConvertJsonDateTimeToChinaDateTime(string dateString)
         {
-            string utcTimeString =
-                new DateTime(1970, 1, 1).AddMilliseconds(Convert.ToInt64(dateString) * 1000)
-                    .ToString("yyyy-MM-dd HH:mm:ss");
-            DateTime chinaTime = DateTime.ParseExact(utcTimeString, "yyyy-MM-dd HH:mm:ss", new CultureInfo("zh-CN"),
-                DateTimeStyles.AssumeUniversal);
-            return chinaTime;
+//            string utcTimeString =
+//                new DateTime(1970, 1, 1).AddMilliseconds(Convert.ToInt64(dateString) * 1000)
+//                    .ToString("yyyy-MM-dd HH:mm:ss");
+//            DateTime chinaTime = DateTime.ParseExact(utcTimeString, "yyyy-MM-dd HH:mm:ss", new CultureInfo("zh-CN"),
+//                DateTimeStyles.AssumeUniversal);
+//            return chinaTime;
+            return
+                TimeZone.CurrentTimeZone.ToLocalTime(
+                    new DateTime(1970, 1, 1).AddMilliseconds(Convert.ToInt64(dateString)*1000));
         }
 
         public static long ConvertDateTimeToJsonTimeStamp(DateTime time)
